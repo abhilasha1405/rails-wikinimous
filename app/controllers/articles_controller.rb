37a2486553b2c4a8018a9583b112d842b074923a
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-04-13 16:17:14 +0200

    Rails Wikinimous

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,35 +1,56 @@
 class ArticlesController < ApplicationController
+
+
   def index
     @articles = Article.all
   end
 
+  # GET /articles/1
   def show
-    @article = Article.find(params[:id])
   end
 
-  def create
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_path(@article)
-  end
-
+  # GET /articles/new
   def new
     @article = Article.new
   end
-
+  # GET /articles/1/edit
   def edit
-    @article = Article.find(params[:id])
   end
 
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /articles/1
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-    redirect_to articles_path
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
-  def article_params
-    params.require('article').permit(:title, :content)
-  end
+    # Only allow a trusted parameter "white list" through.
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new'
+  resources :totos
 
-  get 'article/:id/edit', to: 'articles#edit', as: :edit
-  get 'article/:id', to: 'articles#show', as: :article
+  resources :articles
 
-  delete 'article/:id', to: 'articles#destroy'
-  patch 'article/:id', to: 'articles#update'
-  post 'articles', to: 'articles#create'
+  root to: 'articles#index'
+  end
+  # get 'articles', to: 'articles#index'
+  # get 'articles/new', to: 'articles#new'
+
+  # get 'article/:id/edit', to: 'articles#edit', as: :edit
+  # get 'article/:id', to: 'articles#show', as: :article
+
+  # delete 'article/:id', to: 'articles#destroy'
+  # patch 'article/:id', to: 'articles#update'
+  # post 'articles', to: 'articles#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+
